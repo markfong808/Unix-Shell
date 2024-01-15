25f03f399e56fedd98dc01795ae3ca4089c67a1a
Mark <ipo3678w@gamil.com>
2024-01-15 17:57:19 +0000

    2 functions left

--- shell.c
+++ shell.c
@@ -1,12 +1,17 @@
 #include "shell.h"
 
 int main(int argc, char **argv) {
+  
   if (argc == 2 && equal(argv[1], "--interactive")) {
     return interactiveShell();
   } else {
     return runTests();
   }
+  
 }
+//Global for bookmark history
+char history[MAX_HIS_SIZE][MAXLINE];
+int count = 0;
 
 // interactive shell to process commands
 int interactiveShell() {
@@ -15,8 +20,19 @@ int interactiveShell() {
   while (should_run) {
     printf(PROMPT);
     fflush(stdout);
+    //After reading user input
     int n = fetchline(&line);
     printf("read: %s (length = %d)\n", line, n);
+    
+    //history command bookmark
+    if(equal(line,"!!")){
+      runPrevCommand();   
+    }
+    addTobookmark(line);
+    
+    // Display bookmarks
+    displayBookmarks();
+    
     // ^D results in n == -1
     if (n == -1 || equal(line, "exit")) {
       should_run = false;
@@ -25,13 +41,105 @@ int interactiveShell() {
     if (equal(line, "")) {
       continue;
     }
-    processLine(line);
-  }
+    
+  processLine(line);
+    
+ }
   free(line);
   return 0;
 }
+void addTobookmark(char *line) {
+    if(count < MAX_HIS_SIZE){
+      strncpy(history[count],line,MAXLINE - 1);
+      history[count][MAXLINE - 1] = '\0';
+      count = (count + 1) % MAXLINE;
+    }
+}
+void displayBookmarks() {
+    printf("Bookmarks:\n");
+    for (int i = 0; i < count; i++) {
+      if(history[i][0] != '\0'){
+        printf("%d. %s\n", i + 1, history[i]);
+      }
+    }
+}
+void runPrevCommand(){
+   if(count == 0 || history[count - 1][0] == '\0'){
+    printf("No previos connnand\n");   
+   } else{
+    printf("Run previous command : %s\n",history[count - 1]);        
+    // Implement the code to execute the command here
+    system(history[count - 1]);
+   }
+}
+void processLine(char *line) { 
+  
+  printf("processing line: %s\n",line);
+  char *args[25];
+  parse_input(line,args);
+  
+char current_directory[130];
+if (getcwd(current_directory, sizeof(current_directory)) != NULL) {
+    //printf("Current working directory: %s\n", current_directory);
+} else {
+    perror("getcwd failed");
+}
 
-void processLine(char *line) { printf("processing line: %s\n", line); }
+  pid_t p = fork();
+  if (p == 0) {
+    // Child process  
+    bool concurrenly = false;
+   //name a file descriptor
+    int out_file = -1;
+    int in_file =  -1;
+    for(int i = 0; args[i] != NULL; i++){
+      if(equal(args[i], ">")){
+        out_file = i + 1;
+      }
+      if(equal(args[i], "<")){
+        in_file = i + 1;
+      }
+      // if(equal(args[i],";")){
+      //   args[i]= NULL;// Terminate the command at the semicolon
+      // }
+      //"&" found
+      if(equal(args[i],"&")){
+        concurrenly = true;
+      }
+    }
+     
+     
+    //input redirection
+    if (in_file != -1 && args[in_file] != NULL) {
+        char input_path[PATH_MAX];
+        snprintf(input_path, PATH_MAX, "%s/junk.txt", current_directory); //docker container permission problem
+        int redirect_fd = open(input_path, O_RDONLY);
+        dup2(redirect_fd, STDIN_FILENO); // Connect file descriptor to stdin
+        close(redirect_fd);
+        args[in_file - 1] = NULL; //reset the flag back to -1
+        // args[in_file] = NULL; //removing from the args array
+    }
+    
+    //output redirection
+    if(out_file != -1 && args[out_file] != NULL){
+    int redirect_fd = open(strcat(current_directory, "/junk.txt"), O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU); 
+    dup2(redirect_fd,STDOUT_FILENO); //connect file descriptor into stdout
+    close(redirect_fd);
+    args[out_file - 1] = NULL;
+    // args[out_file] = NULL;
+  }
+  
+  execvp(args[0],args);
+
+   
+  }else {
+    // Parent process  
+      int status;  
+      wait(&status);
+  }
+  
+}   
+
 
 int runTests() {
   printf("*** Running basic tests ***\n");
@@ -56,7 +164,23 @@ int fetchline(char **line) {
   size_t len = 0;
   size_t n = getline(line, &len, stdin);
   if (n > 0) {
+    //replace the last character string with NULL
     (*line)[n - 1] = '\0';
   }
   return n;
 }
+
+void parse_input(char *input,char *args[]){
+    int args_count = 0;
+    char *tokens[25];
+    char *pch = strtok(input," ");//ex:return "ls"
+    // char *ampersend = strtok(input,"&");
+    while(pch != NULL){
+      // char *ampersend = strtok(input,"&");
+      printf("Token [%d]: %s\n", args_count, pch);
+      args[args_count++] = pch;
+      pch = strtok(NULL," ");
+    }   
+      args[args_count] = NULL; // Null-terminate the array
+  }
+
--- shell.h
+++ shell.h
@@ -8,18 +8,25 @@
 #include <stdlib.h>  // calloc
 #include <string.h>  // strcmp
 #include <unistd.h>  // execvp
+#include <sys/wait.h>
+#include <sys/stat.h>
+
 
 #define MAXLINE 80
 #define PROMPT "osh> "
-
+#define MAX_HIS_SIZE 10
+#define PATH_MAX 100
 #define RD 0
 #define WR 1
-
+void checkFlags(char *line);
 bool equal(char *a, char *b);
 int fetchline(char **line);
 int interactiveShell();
 int runTests();
 void processLine(char *line);
+void addTobookmark(char *line);
+void displayBookmarks();
+void runPrevCommand();
+void parse_input();
 int main();
-
 #endif
